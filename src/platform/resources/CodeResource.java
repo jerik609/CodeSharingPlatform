9549ad3b4f62c5ca86760332186296fab257c39b
Michal <117671671+jerik609@users.noreply.github.com>
2023-04-09 21:49:09 +0200

    add thymeleaf configuration

--- src/platform/resources/CodeResource.java
+++ src/platform/resources/CodeResource.java
@@ -1,14 +1,29 @@
 package platform.resources;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+// https://medium.com/@nandaras0103/spring-mvc-and-thymleaf-fa48c000f68a
+
+// https://www.thymeleaf.org/documentation.html
 
 @RestController
 public class CodeResource {
 
     @GetMapping("/code")
-    public String getCode() {
-        return "";
+    public ResponseEntity<String> getCode() {
+        final var headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_HTML);
+
+        final var response = new ResponseEntity<String>(
+            "<h1>hello</h1>", headers, HttpStatus.OK
+        );
+
+        return response;
     }
 
 }
